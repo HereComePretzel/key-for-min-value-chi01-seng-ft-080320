4c68b71550f59477bfa701c5c91fee877aaf662e
Austin Oie <austinoie@gmail.com>
2020-07-01 12:42:41 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,6 +7,10 @@ def key_for_min_value(name_hash)
   name_hash.each do |key, value|
     if lowest == nil  
     lowest = value
+    lowest_key = key
+  else 
+    value < lowest 
+    lowest = value
     lowest_key = key 
     end 
   end 
